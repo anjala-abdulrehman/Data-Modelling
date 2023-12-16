b8674c232069ddf2230ce4adbbe8d2ea32398502
anjala-abdulrehman <anjalaar20@gmail.com>
2023-12-16 15:04:49 +0530

    assignment-submission-flink

---
+++ infrastructure/apache_flink_training/src/submission/average_web_events_ddl.sql
@@ -0,0 +1,11 @@
+-- Define a table to store the average number of web events in a session by hostname (average_web_events_ddl)
+
+
+-- Define a table to store the average number of web events in a session by hostname (average_web_events_ddl)
+
+CREATE TABLE average_web_events_ddl (
+    dim_host_name VARCHAR,
+    m_avg_num_web_events FLOAT
+)
+
+
